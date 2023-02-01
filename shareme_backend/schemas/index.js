39ad9252a13c59b3091a4d52264c1fc10f98a0ad
Fuji014 <jeromegabat14@gmail.com>
2023-02-01 17:29:35 +0800

    add files to backend schema

--- shareme_backend/schemas/index.js
+++ shareme_backend/schemas/index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import user from './user'
+import comment from './comment'
+import pin from './pin'
+import save from './save'
+import postedBy from './postedBy'
+
+export const schemaTypes = [user, comment, pin, save, postedBy]
